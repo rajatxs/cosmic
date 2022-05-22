0f1d1f52b1a8622e1439ea3f568479b3ec4615da
Rajat Sharma <rxx175@gmail.com>
2022-05-22 22:19:52 +0530

    crypto

--- main.go
+++ main.go
@@ -1,13 +1,4 @@
 package main
 
-import (
-	"github.com/rajatxs/cosmic/ledger"
-)
-
 func main() {
-	// sh := core.NewSlotHeader(5)
-	// ledger.InsertSlotHeader(&sh)
-
-	// fmt.Println(sh)
-	ledger.GetSlotHeaderBySeq(5)
 }
