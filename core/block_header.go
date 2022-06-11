5d506baaff5ede8317f0de1d7aee956de01eb1a4
Rajat Sharma <rxx175@gmail.com>
2022-06-11 16:21:34 +0530

    Update core

--- core/block_header.go
+++ core/block_header.go
@@ -5,45 +5,45 @@ import (
 
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/rajatxs/cosmic/codec"
-	"github.com/rajatxs/cosmic/core/ctypes"
 	"github.com/rajatxs/cosmic/crypto"
+	"github.com/rajatxs/cosmic/ctype"
 	"github.com/rajatxs/cosmic/logger"
 )
 
 type BlockHeader struct {
-	Id             uint64      `json:"id"`
-	Sig            ctypes.Hash `json:"sig"`
-	Height         uint64      `json:"height"`
-	Version        uint16      `json:"version"`
-	GasUsed        uint64      `json:"gasUsed"`
-	Reward         uint64      `json:"reward"`
-	TotalTx        uint16      `json:"totalTransactions"`
-	StateSig       ctypes.Hash `json:"stateSignature"`
-	TxSig          ctypes.Hash `json:"txSignature"`
-	ParentBlockSig ctypes.Hash `json:"parentBlockSig"`
-	Time           uint64      `json:"time"`
+	Id              uint64          `json:"id"`
+	Code            ctype.BlockCode `json:"code"`
+	Height          uint64          `json:"height"`
+	Version         uint16          `json:"version"`
+	GasUsed         uint64          `json:"gasUsed"`
+	Reward          uint64          `json:"reward"`
+	TotalTx         uint16          `json:"totalTransactions"`
+	StateCode       ctype.HashCode  `json:"stateCode"`
+	TxCode          ctype.HashCode  `json:"txCode"`
+	ParentBlockCode ctype.BlockCode `json:"parentBlockCode"`
+	Time            uint64          `json:"time"`
 }
 
 func (bh *BlockHeader) IsEmpty() bool {
 	return (bh.Id == 0 ||
 		bh.Version == 0 ||
 		bh.Height == 0 ||
-		len(bh.ParentBlockSig) == 0 ||
-		len(bh.StateSig) == 0 ||
-		len(bh.TxSig) == 0 ||
+		len(bh.ParentBlockCode) == 0 ||
+		len(bh.StateCode) == 0 ||
+		len(bh.TxCode) == 0 ||
 		bh.Time < 1)
 }
 
 func NewBlockHeader(id uint64) BlockHeader {
 	return BlockHeader{
-		Id:             id,
-		Version:        0,
-		Height:         id - 1,
-		ParentBlockSig: codec.NilSha256Bytes,
-		StateSig:       codec.NilSha256Bytes,
-		TxSig:          codec.NilSha256Bytes,
-		GasUsed:        0,
-		Time:           0,
+		Id:              id,
+		Version:         0,
+		Height:          id - 1,
+		ParentBlockCode: codec.NilSha256Bytes,
+		StateCode:       codec.NilSha256Bytes,
+		TxCode:          codec.NilSha256Bytes,
+		GasUsed:         0,
+		Time:            0,
 	}
 }
 
@@ -55,6 +55,29 @@ func (bh *BlockHeader) EncodeRLP() []byte {
 	}
 
 	return encoded
+}
+
+func (bh *BlockHeader) Encode(r *[]byte) error {
+	enc := codec.NewByteEncoder(140)
+
+	enc.WriteUint64(bh.Id)
+	enc.WriteUint64(bh.Height)
+	enc.WriteUint16(bh.Version)
+	enc.WriteUint64(bh.GasUsed)
+	enc.WriteUint64(bh.Reward)
+	enc.WriteUint16(bh.TotalTx)
+	enc.WriteBytes(bh.StateCode[:])
+	enc.WriteBytes(bh.TxCode[:])
+	enc.WriteBytes(bh.ParentBlockCode[:])
+	enc.WriteUint64(bh.Time)
+
+	if enc.Error != nil {
+		return enc.Error
+	}
+
+	*r = enc.Bytes
+
+	return nil
 }
 
 func (bh *BlockHeader) VerifySig(sig *[]byte) bool {
